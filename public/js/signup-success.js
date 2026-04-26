398341f71be3b7b215c93bb5d9b914d298e8cefa
Phatcraft <phathong0727@gmail.com>
2026-04-26 13:52:38 +0700

    scripts

---
+++ public/js/signup-success.js
@@ -0,0 +1,20 @@
+var messagebox = document.getElementById("message")
+var submit = document.getElementById("submit")
+
+messagebox.classList.replace("bg-danger", "bg-success")
+messagebox.classList.replace("d-none", "d-block")
+submit.disabled = true
+
+messagebox.innerHTML = "Chào mừng đến với Lunarbox.<br>Đang chuyển về trang chủ trong <strong>3</strong>s"
+setTimeout(() => {
+    messagebox.innerHTML = "Chào mừng đến với Lunarbox.<br>Đang chuyển về trang chủ trong <strong>2</strong>s"
+    setTimeout(() => {
+        messagebox.innerHTML = "Chào mừng đến với Lunarbox.<br>Đang chuyển về trang chủ trong <strong>1</strong>s"
+        setTimeout(() => {
+            messagebox.innerHTML = "Đang chuyển trang . . . "
+            setTimeout(() => {
+                location.replace("/")
+            },500)
+        }, 1000)
+    }, 1000)
+},1000)
---
+++ public/js/wrong-password-confirm.js
@@ -0,0 +1,3 @@
+var messagebox = document.getElementById("message")
+messagebox.classList.replace("d-none", "d-block")
+messagebox.innerHTML = "Mật khẩu và xác nhận mật khẩu không khớp"
