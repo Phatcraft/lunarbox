2d20d3b82ff3a0af4df991c2899367d6f09a2bb4
Phatcraft <phathong0727@gmail.com>
2026-04-26 14:01:37 +0700

    scripts

--- public/js/login-success.js
+++ public/js/login-success.js
@@ -1,16 +1,16 @@
 var messagebox = document.getElementById("message")
 var submit = document.getElementById("submit")
 
-messagebox.classList.replace("bg-danger", "bg-success")
+messagebox.classList.replace("bg-danger", "bg-primary")
 messagebox.classList.replace("d-none", "d-block")
 submit.disabled = true
 
 // Counter
-messagebox.innerHTML = "Đăng nhập thành công!<br>Đang chuyển về trang chủ trong <strong>3</strong>s"
+messagebox.innerHTML = "Đăng nhập thành công! Đang chuyển về trang chủ trong <strong>3</strong>s"
 setTimeout(() => {
-    messagebox.innerHTML = "Đăng nhập thành công!<br>Đang chuyển về trang chủ trong <strong>2</strong>s"
+    messagebox.innerHTML = "Đăng nhập thành công! Đang chuyển về trang chủ trong <strong>2</strong>s"
     setTimeout(() => {
-        messagebox.innerHTML = "Đăng nhập thành công!<br>Đang chuyển về trang chủ trong <strong>1</strong>s"
+        messagebox.innerHTML = "Đăng nhập thành công! Đang chuyển về trang chủ trong <strong>1</strong>s"
         setTimeout(() => {
             messagebox.innerHTML = "Đang chuyển trang . . . "
             setTimeout(() => {
--- public/js/signup-success.js
+++ public/js/signup-success.js
@@ -1,15 +1,15 @@
 var messagebox = document.getElementById("message")
 var submit = document.getElementById("submit")
 
-messagebox.classList.replace("bg-danger", "bg-success")
+messagebox.classList.replace("bg-danger", "bg-primary")
 messagebox.classList.replace("d-none", "d-block")
 submit.disabled = true
 
-messagebox.innerHTML = "Chào mừng đến với Lunarbox.<br>Đang chuyển về trang chủ trong <strong>3</strong>s"
+messagebox.innerHTML = "Chào mừng đến với Lunarbox. Đang chuyển về trang chủ trong <strong>3</strong>s"
 setTimeout(() => {
-    messagebox.innerHTML = "Chào mừng đến với Lunarbox.<br>Đang chuyển về trang chủ trong <strong>2</strong>s"
+    messagebox.innerHTML = "Chào mừng đến với Lunarbox. Đang chuyển về trang chủ trong <strong>2</strong>s"
     setTimeout(() => {
-        messagebox.innerHTML = "Chào mừng đến với Lunarbox.<br>Đang chuyển về trang chủ trong <strong>1</strong>s"
+        messagebox.innerHTML = "Chào mừng đến với Lunarbox. Đang chuyển về trang chủ trong <strong>1</strong>s"
         setTimeout(() => {
             messagebox.innerHTML = "Đang chuyển trang . . . "
             setTimeout(() => {
